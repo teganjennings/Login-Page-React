bf94c4dda92dd156e402a8fe104eefacd0654a1d
Tegan Jennings <teganjennings1@hotmail.com>
2024-02-24 13:29:22 +0000

    add: dynamic styled components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -14,7 +14,7 @@ const Label = styled.label`
 	font-weight: 700;
 	letter-spacing: 0.1em;
 	text-transform: uppercase;
-	color: #6b7280;
+	color: ${(invalid) => (invalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
@@ -27,6 +27,7 @@ const Input = styled.input`
 	border-radius: 0.25rem;
 	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
+
 export default function AuthInputs() {
 	const [enteredEmail, setEnteredEmail] = useState("");
 	const [enteredPassword, setEnteredPassword] = useState("");
@@ -51,9 +52,7 @@ export default function AuthInputs() {
 		<div id="auth-inputs">
 			<ControlContainer>
 				<p>
-					<Label className={`label ${emailNotValid ? "invalid" : ""}`}>
-						Email
-					</Label>
+					<Label invalid={emailNotValid}>Email</Label>
 					<Input
 						type="email"
 						// style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
